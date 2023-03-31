7a670b29b6cd4c3d72c70802963bcc19c145c322
Parag Jadhav <parag242001@gmail.com>
2023-03-31 13:54:55 +0000

    Update 19 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./styles.css";
+import "./App.css";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Hiro from "./components/Hiro";
+import Skills from "./components/Skills";
+import Honors from "./components/Honors";
+import Certs from "./components/Certs";
+import Footer from "./components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-export default function App() {
+function App() {
+  useEffect(() => {
+    document.title = "Damasukma Trihanandi";
+    AOS.init();
+  }, []);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="px-6 lg:px-20 xl:px-36 bg-dark-500">
+      <Navbar />
+      <Hiro />
+      <Skills />
+      <Honors />
+      <Certs />
+      <Footer />
     </div>
   );
 }
+
+export default App;
