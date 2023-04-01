0a7b3ec8ff336954dbc1635a2fc26a74d519f15d
Parag Jadhav <parag242001@gmail.com>
2023-04-01 04:38:21 +0000

    Updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,34 +1,29 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import SkillCard from './SkillCard.js';
-
-import javascript from '../assets/skills/javascript.svg';
-import bash from '../assets/skills/bash.svg';
-import kvm from '../assets/skills/kvm.svg';
-import linux from '../assets/skills/linux.svg';
-import python from '../assets/skills/python.svg';
-import reactIcon from '../assets/skills/react.svg';
-import tailwind from '../assets/skills/tailwind.svg';
-import mikrotik from '../assets/skills/mikrotik.svg';
-import kubernetes from '../assets/skills/kubernetes.svg';
-import ansible from '../assets/skills/ansible.svg';
-import windows from '../assets/skills/windows.svg';
-import react from '../assets/skills/react.svg';
-import cisco from '../assets/skills/cisco.svg';
+import 'slick-carousel/slick/slick.css';
+import aws from '../assets/skills/aws.svg';
+import azure from '../assets/skills/azure.svg';
 import docker from '../assets/skills/docker.svg';
-import git from '../assets/skills/git.svg';
 import express from '../assets/skills/express.svg';
-import gitlab from '../assets/skills/gitlab.svg';
-
+import firebase from '../assets/skills/firebase.svg';
+import gcp from '../assets/skills/gcp.svg';
+import git from '../assets/skills/git.svg';
 import hr from '../assets/curve-hr.svg';
+import mongodb from '../assets/skills/mongodb.svg';
+import nodejs from '../assets/skills/nodejs.svg';
+import reactIcon from '../assets/skills/react.svg';
+import redis from '../assets/skills/redis.svg';
+import SkillCard from './SkillCard.js';
+import Slider from 'react-slick';
+import sql from '../assets/skills/sql.svg';
+import microservice from '../assets/skills/microservice.svg';
+import serverless from '../assets/skills/serverless.svg';
 
 export default function Skills() {
   const settings = {
     dots: false,
     autoplay: true,
     infinite: true,
-    slidesToShow: 2,
+    slidesToShow: 4,
     slidesToScroll: 1
   };
 
@@ -39,26 +34,24 @@ export default function Skills() {
 
       <div className="mt-4">
         <Slider {...settings}>
-          <SkillCard name="linux" experience="1 years" img={linux} />
-          <SkillCard name="bash" experience="1 years" img={bash} />
-          <SkillCard name="python" experience="1 years" img={python} />
-          <SkillCard name="javascript" experience="1 years" img={javascript} />
-          <SkillCard name="react" experience="1 years" img={reactIcon} />
-          <SkillCard name="tailwind" experience="1 years" img={tailwind} />
-          <SkillCard name="kvm" experience="1 years" img={kvm} />
-          <SkillCard name="mikrotik" experience="1 years" img={mikrotik} />
-          <SkillCard name="kubernetes" experience="1 years" img={kubernetes} />
-          <SkillCard name="ansible" experience="1 years" img={ansible} />
-          <SkillCard name="windows" experience="1 years" img={windows} />
-          <SkillCard name="react" experience="1 years" img={react} />
-          <SkillCard name="cisco" experience="1 years" img={cisco} />
-          <SkillCard name="docker" experience="1 years" img={docker} />
-          <SkillCard name="git" experience="1 years" img={git} />
-          <SkillCard name="express" experience="1 years" img={express} />
-          <SkillCard name="gitlab" experience="1 years" img={gitlab} />
+          <SkillCard name="NodeJs" experience="6 years" img={nodejs} />
+          <SkillCard name="ExpressJs" experience="6 years" img={express} />
+          <SkillCard name="MongoDB" experience="6 years" img={mongodb} />
+          <SkillCard name="Redis" experience="4 years" img={redis} />
+          <SkillCard name="AWS" experience="6 years" img={aws} />
+          <SkillCard name="GCP" experience="3 years" img={gcp} />
+          <SkillCard name="Azure" experience="2 years" img={azure} />
+          <SkillCard name="Firebase" experience="3 years" img={firebase} />
+          <SkillCard name="MySQL" experience="5 years" img={sql} />
+          <SkillCard name="Microservices" experience="4 years" img={microservice} />
+          <SkillCard name="Serverless" experience="6 years" img={serverless} />
+          <SkillCard name="React" experience="3 years" img={reactIcon} />
+          <SkillCard name="Docker" experience="2 years" img={docker} />
+          <SkillCard name="Git" experience="7 years" img={git} />
         </Slider>
       </div>
       <img src={hr} className="w-full mt-8 md:h-3" alt="hr" />
+      <br />
     </div>
   );
 }
